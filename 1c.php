9af71b1cbb5f13951934ca0e501eb06b6e9519ef
Rizkirq <rizki.223040167@mail.unpas.ac.id>
2023-02-22 10:08:57 +0700

    tugas kuliah

---
+++ 1c.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .kotak1{
+            height: 80px;
+            width: 80px;
+            background-color: lightblue;
+            border: 2px solid black;
+            line-height: 80px;
+            text-align: center;
+        }
+        .kotak2{
+            height: 80px;
+            width: 80px;
+            background-color: lightblue;
+            border: 2px solid black;
+            line-height: 80px;
+            text-align: center;
+            float: left;
+        }
+        .kotak3{
+            height: 80px;
+            width: 80px;
+            background-color: lightblue;
+            border: 2px solid black;
+            line-height: 80px;
+            text-align: center;
+            float: left;
+        }
+        .clear{
+            clear: left;
+        }
+    </style>
+</head>
+<body>
+    <div class="kotak1">1</div>
+    <div class="kotak2">2</div>
+    <div class="kotak2">2</div>
+    <div class="clear"></div>
+    <div class="kotak3">3</div>
+    <div class="kotak3">3</div>
+    <div class="kotak3">3</div>
+    
+</body>
+</html>
